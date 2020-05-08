189527b94b07e32803001ff83bb6c12f634163aa
eacarras <enriqueaaron7@gmail.com>
2020-05-07 23:41:18 -0500

    Create initial routes with his functions

--- api_rest/main.go
+++ api_rest/main.go
@@ -8,7 +8,10 @@ import ("fmt"
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true);
+	// Routes
 	router.HandleFunc("/health", Health);
+	router.HandleFunc("/persons", PersonsList);
+	router.HandleFunc("/persons/{id}", PersonDetails);
 
 	fmt.Println("Running server in http://localhost:2020");
 	server := http.ListenAndServe(":2020", router) ;
@@ -19,4 +22,17 @@ func main() {
 func Health(resp http.ResponseWriter, req *http.Request) {
 	// TODO: Improve this health func
 	fmt.Fprintf(resp, "Server is up and running...");
+}
+
+func PersonsList(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Getting list of persons...");
+	fmt.Fprintf(resp, "Persons: ");
+}
+
+func PersonDetails(resp http.ResponseWriter, req *http.Request) {
+	parms := mux.Vars(req);
+	person_id := parms["id"];
+
+	fmt.Printf("Getting the information of a person with id: %s...", person_id);
+	fmt.Fprintf(resp, "Person Detail: ");
 }
