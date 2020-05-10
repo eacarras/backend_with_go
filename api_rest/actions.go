57637b9738894055a6d950573f805c27c97da8b3
eacarras <enriqueaaron7@gmail.com>
2020-05-10 18:05:10 -0500

    Search one person for his objectId

--- api_rest/actions.go
+++ api_rest/actions.go
@@ -7,7 +7,7 @@ import (
 	"encoding/json"
 	"log"
 	"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func get_session() *mgo.Session {
@@ -48,7 +48,27 @@ func PersonDetails(resp http.ResponseWriter, req *http.Request) {
 	parms := mux.Vars(req);
 	person_id := parms["id"];
 
-	fmt.Printf("Getting the information of a person with id: %s...", person_id);
+	fmt.Printf("Getting the information of a person with id: %s...\n", person_id);
+
+	if !bson.IsObjectIdHex(person_id) {
+		resp.WriteHeader(404)
+		return
+	}
+
+	decoded_id := bson.ObjectIdHex(person_id)
+	
+	result := person{}
+	err := collection.FindId(decoded_id).One(&result)
+
+	if err != nil {
+		log.Fatal(err)
+		resp.WriteHeader(404)
+		return
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(200)
+	json.NewEncoder(resp).Encode(result)
 }
 
 func PersonAdd(resp http.ResponseWriter, req *http.Request) {
